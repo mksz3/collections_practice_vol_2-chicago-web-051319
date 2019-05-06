d41a745049a79e1c1de8dd7f86ada6dab12ce5a9
Markus Sanders <markus.sanderst@gmail.com>
2019-05-06 20:58:24 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -21,8 +21,8 @@ def remove_non_strings(arr)
 end
 
 def count_elements(arr) 
-  counts = Hash.new
-  arr.map {|element| counts[:element] += 1 }
+  counts = Hash.new(0)
+  arr.map {|element| counts[element] += 1 }
     counts.map { |hash, count| hash[:count] = count }
   return counts.keys
 end
